442d043dadc342f1e5b64c39f02a188e2df21797
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-12 13:27:00 +0200

    Create basic logic with user input

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ const container = document.querySelector(".container");
 const columns = [];
 const divs = [];
 
+restartGame();
 
-for (let i = 0; i < 4; i++) {
-    const column = document.createElement("div");
-    column.classList.toggle("column");
+function restartGame() {
 
-    for (let j = 0; j < 4; j++) {
-        const div = document.createElement("div");
-        div.classList.toggle("item");
-        column.appendChild(div);
-        divs.push(div);
+    let amount = Number(prompt("Which amount of squares per line do you want?", "4"));
+    if (isNaN(amount)) {
+        amount = 4;
     }
-    container.appendChild(column);
-    columns.push(column);
+
+    for (const col of columns) {
+        for (const d of divs) {
+            d.remove();
+        }
+        col.remove();
+    }
+
+    for (let i = 0; i < amount; i++) {
+        const column = document.createElement("div");
+        column.classList.toggle("column");
+    
+        for (let j = 0; j < amount; j++) {
+            const div = document.createElement("div");
+            div.classList.toggle("item");
+            column.appendChild(div);
+            divs.push(div);
+        }
+        container.appendChild(column);
+        columns.push(column);
+    }
 }
+
+
+
 
